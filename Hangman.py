ef77718c80dbc821f57509b9f5be4f04f6915551
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-20 21:05:44 -0500

    updated with new names from Game

--- Hangman.py
+++ Hangman.py
@@ -6,41 +6,36 @@ from Player import Player
 
 from Console import *
 
-# two random letter generators battle it out!
-# Computer 1 vs. Computer 2 battling it out in the oldest game in the books.
-# who will wither away, who will reign supreme? who can stand this cliché??
-
 def main():
 	game = Game()
 	console = Console()
 
-	wrongGuessesAllowed = game.wrongGuessesPerPlayer
-	player1 = Player("Computer 1", wrongGuessesAllowed)
-	player2 = Player("Computer 2", wrongGuessesAllowed)
+	player1 = Player("Computer 1")
+	player2 = Player("Computer 2")
 	players = []
 	players.append(player1)
 	players.append(player2)
 
 	console.printStartupSequence()
-	console.printScoreboardForGame(game)
+	console.printScoreboardFor(game)
 
 	numPlayers = len(players)
 	currentPlayerId = 0
 	while (game.gameInPlay):
 		currentPlayer = players[currentPlayerId]
 
-		console.retrieveGuessFromPlayer(currentPlayer)
+		console.retrieveGuessFrom(currentPlayer)
 
-		game.takeTurnForPlayer(currentPlayer)
-		console.printGuessForPlayer(currentPlayer)
-		console.printGuessedLettersForGame(game)
+		game.takeTurnFor(currentPlayer)
+		console.printGuessFor(currentPlayer)
+		console.printGuessedLettersFor(game)
 		if not game.didPlayerJustWin(currentPlayer):
-			console.printStatusForPlayer(currentPlayer)
-		console.printScoreboardForGame(game)
+			console.printStatusFor(currentPlayer)
+		console.printScoreboardFor(game)
 
-		if (game.shouldEndForPlayer(currentPlayer)):
+		if (game.shouldEndFor(currentPlayer)):
 			game.gameInPlay = False
-			console.printGameOverMessageForPlayer(currentPlayer)
+			console.printGameOverMessageFor(currentPlayer)
 		currentPlayerId = (currentPlayerId + 1) % numPlayers
 	sys.exit()
 
